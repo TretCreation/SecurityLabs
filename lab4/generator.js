36bc3e03fa0177b4c864bf83c520d0d5747ec844
TretCreation <tret.creation@gmail.com>
2022-01-15 22:29:22 +0200

    unoptimized first part of the code

--- lab4/generator.js
+++ lab4/generator.js
@@ -1,4 +1,6 @@
 const fs = require('fs');
+const crypto = require('crypto');
+const argon2 = require('argon2');
 
 let commonWords = fs.readFileSync('res/commonWords.txt', 'utf8').split('\r\n');
 let top100Pas = fs.readFileSync('res/top100Passwords.txt', 'utf8').split('\r\n');
@@ -22,42 +24,68 @@ const randomInteger = (min, max) => {
 }
 // console.log(randomInteger(1, 10))
 
-const generatePasswords = () => {
+async function generatePasswords () {
 	const allPas = 100000;
 	let password;
 	let passwords = [];
+	let passwordsMD5 = []
+	let passwordsArgon2 = [];
+	let hashMD5;
+	let hashArgon2;
 	const percent1 = Math.floor(randomInteger(5, 10) * top100Pas.length / 100);
-	console.log("percent1", percent1);
+	// console.log("percent1", percent1);
 	const percent2 = Math.floor(randomInteger(50, 90) * top100kPas.length / 100);
-	console.log("percent2", percent2);
+	// console.log("percent2", percent2);
 	const percent3 = Math.floor(randomInteger(1, 5) * commonWords.length / 100);
-	console.log("percent3", percent3);
+	// console.log("percent3", percent3);
 	const ost = allPas - percent1 - percent2 - percent3;
-	console.log("ost", ost);
+	// console.log("ost", ost);
 
 	for (let i=0; i < percent1; i++) {
 		password = top100Pas[randomInteger(0, top100Pas.length - 1)]
-		passwords.push(password);
+		hashMD5 = crypto.createHash('md5').update(password).digest('hex');
+		hashArgon2 = await argon2.hash(password, {type: argon2.argon2d});
+		passwordsMD5.push(hashArgon2);
+		passwordsArgon2.push(hashMD5);
+		// passwords.push(password);
 	}
 	for (let i=0; i < percent2; i++) {
 		password = top100kPas[randomInteger(0, top100kPas.length - 1)]
-		passwords.push(password);
+		hashMD5 = crypto.createHash('md5').update(password).digest('hex');
+		hashArgon2 = await argon2.hash(password, {type: argon2.argon2d});
+		passwordsMD5.push(hashArgon2);
+		passwordsArgon2.push(hashMD5);
+		// passwords.push(password);
 	}
 	for (let i=0; i < percent3; i++) {
 		password = commonWords[randomInteger(0, commonWords.length - 1)] + stongGeneratePassword(4);
-		passwords.push(password);
+		hashMD5 = crypto.createHash('md5').update(password).digest('hex');
+		hashArgon2 = await argon2.hash(password, {type: argon2.argon2d});
+		passwordsMD5.push(hashArgon2);
+		passwordsArgon2.push(hashMD5);
+		// passwords.push(password);
 	}
 	for (let i=0; i < ost; i++) {
 		password = stongGeneratePassword(10);
-		passwords.push(password);
+		hashMD5 = crypto.createHash('md5').update(password).digest('hex');
+		hashArgon2 = await argon2.hash(password, {type: argon2.argon2d});
+		passwordsMD5.push(hashArgon2);
+		passwordsArgon2.push(hashMD5);
+		// passwords.push(password);
 	}
 
-	fs.writeFileSync('./res/arrPas.txt', passwords.join('\n'));
-	return passwords;
+	fs.writeFileSync('./res/hashMD5.csv', passwordsMD5.join('\n'));
+	fs.writeFileSync('./res/hashArgon2.csv', passwordsArgon2.join('\n'));
+	// fs.writeFileSync('./res/commonWords.txt', passwords.join('\n'));
+	
+	console.log(passwords);
+	console.log(passwordsMD5);
+	console.log(passwordsArgon2)
+	// return passwords;
 }
 
-// generatePasswords();
+generatePasswords();
 
-module.exports = {
-	generatePasswords
-}
+// module.exports = {
+// 	generatePasswords
+// }
---
+++ lab4/hash.js
@@ -0,0 +1,14 @@
+// const generator = require('./generator');
+// const fs = require('fs');
+// const crypto = require('crypto');
+
+// // const uint16 = Int16Array.from(generator.generatePasswords());
+// let hash = crypto.createHash('md5').update(generator.generatePasswords()).digest('hex');
+// // for (let i = 0; i < )
+
+
+// console.log(generator.generatePasswords());
+// console.log(hash);
+
+
+// //fs.writeFileSync('./res/hashMD5.txt', hash.join('\n'));
